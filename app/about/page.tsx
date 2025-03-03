ae36b4238b1a4610df2a2b13af5f3ccb8dbbdb29
adeoyetunmise <adeoyetunmise53@gmail.com>
2025-03-03 11:19:45 +0100

    Getting started with KainKain project

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Responsive</button>
+    </div>
+  )
+}
+
+export default page
