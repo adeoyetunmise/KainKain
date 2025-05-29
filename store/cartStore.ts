dd30ab362143fc53cc7164aedcfca6c59c327ab2
adeoyetunmise <adeoyetunmise53@gmail.com>
2025-05-29 15:40:36 +0100

    added comments

--- store/cartStore.ts
+++ store/cartStore.ts
@@ -1,24 +1,26 @@
-import { create } from "zustand";
-
+import { create } from 'zustand';
+// This file defines a Zustand store for managing a shopping cart in a React application.
+// It allows adding items to the cart and keeps track of their quantities.
 interface CartState {
-    cartItems: { slug: string; title: string; price: string; img: string; quantity: number }[];
-    addToCart: (product: { slug: string; title: string; price: string; img: string }) => void;
-  }
+  cartItems: { slug: string; title: string; price: string; img: string; quantity: number }[];
+  addToCart: (product: { slug: string; title: string; price: string; img: string }) => void;
+}
 
-export const useCartStore =  create<CartState>((set) => ({
-    cartItems: [],
+export const useCartStore = create<CartState>((set) => ({
+  cartItems: [],
 
-    addToCart: (product) => set((state) => {
-        const existingProduct = state.cartItems.find((item) => item.slug === product.slug);
-   
-        if (existingProduct) {
-            return {
-                cartItems: state.cartItems.map((item) => item.slug === product.slug ? { ...item, quantity: item.quantity + 1 } : item)
-            }
-        } else {
-            return { cartItems: [...state.cartItems, { ...product, quantity: 1 }] }
-        }
+  addToCart: (product) =>
+    set((state) => {
+      const existingProduct = state.cartItems.find((item) => item.slug === product.slug);
 
-    })
-
-}))
+      if (existingProduct) {
+        return {
+          cartItems: state.cartItems.map((item) =>
+            item.slug === product.slug ? { ...item, quantity: item.quantity + 1 } : item
+          ),
+        };
+      } else {
+        return { cartItems: [...state.cartItems, { ...product, quantity: 1 }] };
+      }
+    }),
+}));
